977d21a9112933cfe143005c47c2fbfe3ca41cce
Mathieu Maes <mathieu@webberig.be>
2014-02-03 22:16:16 +0100

    edit page basic layout

---
+++ Controller/ContentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ConcertoCms\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ContentController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('ConcertoCmsCoreBundle:Content:index.html.twig');
+    }
+}
