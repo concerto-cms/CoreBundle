a58b175d252f022ebbd0b7020025ca2208aef99e
Mathieu Maes <mathieu@webberig.be>
2015-01-06 21:07:39 +0100

    fix command errors, added logo to the app header

--- src/Util/Command/FixturesLoadCommand.php
+++ src/Util/Command/FixturesLoadCommand.php
@@ -11,6 +11,7 @@ namespace ConcertoCms\CoreBundle\Util\Command;
 use ConcertoCms\CoreBundle\Document\SimplePage;
 use ConcertoCms\CoreBundle\Languages\Model\Locale;
 use ConcertoCms\CoreBundle\Languages\Service\LanguagesManager;
+use ConcertoCms\CoreBundle\Navigation\Service\NavigationManager;
 use ConcertoCms\CoreBundle\Pages\Service\PagesManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Cmf\Bundle\MenuBundle\Doctrine\Phpcr\MenuNode;
@@ -22,12 +23,14 @@ class FixturesLoadCommand extends ContainerAwareCommand
 {
     private $pm;
     private $lm;
+    private $nm;
 
-    public function __construct(PagesManager $pm, LanguagesManager $lm)
+    public function __construct(PagesManager $pm, LanguagesManager $lm, NavigationManager $nm)
     {
         parent::__construct();
         $this->pm = $pm;
         $this->lm = $lm;
+        $this->nm = $nm;
     }
     protected function configure()
     {
@@ -88,5 +91,27 @@ class FixturesLoadCommand extends ContainerAwareCommand
             ]);
         $this->pm->flush();
         $this->pm->clear();
+
+        // Navigation
+        $menu = new Menu();
+        $menu->setLabel("Main menu");
+        $menu->setName("main-menu");
+        $this->nm->addMenu($menu);
+        $this->nm->flush();
+        $this->nm->clear();
+
+        $menuItem = new MenuNode();
+        $menuItem->setLabel("Home");
+        $menuItem->setName("home");
+        $menuItem->setRoute("/cms/routes/en");
+        $this->nm->addMenuItem("main-menu", "en", "/", $menuItem);
+
+        $menuItem = new MenuNode();
+        $menuItem->setLabel("Contact");
+        $menuItem->setName("contact");
+        $menuItem->setRoute("/cms/routes/en/contact");
+        $this->nm->addMenuItem("main-menu", "en", "/", $menuItem);
+        $this->nm->flush();
+
     }
 }
